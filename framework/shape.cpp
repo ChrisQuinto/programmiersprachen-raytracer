3ce63373c6b8be6da56afe4c2af7337d26a1e582
ChrisQuinto <ch.paffrath@gmx.net>
2015-08-21 16:56:00 +0200

    little fix

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -1,20 +1,19 @@
-#include <glm/gtx/intersect.hpp>
 #include "shape.hpp"
 
 Shape::Shape() :
     name_("defaultshape"),
-    material_(Material()) {
-        std::cout << "Default-Konstruktor-Aufruf fuer: " << name() << "\n";
+    material_{Material{}} {
+        // std::cout << "Default-Konstruktor-Aufruf fuer: " << name() << "\n";
     }
 
 Shape::Shape(std::string const& n, Material const& m) :
     name_(n),
-    material_(m) {
-        std::cout << "Konstruktor-Aufruf fuer: " << name() << "\n";
+    material_{m} {
+        // std::cout << "Konstruktor-Aufruf fuer: " << name() << "\n";
     }
 
 Shape::~Shape() {
-        std::cout << "Destruktor-Aufruf fuer: " << name() << "\n";
+        // std::cout << "Destruktor-Aufruf fuer: " << name() << "\n";
 }
 
 std::string const& Shape::name() const {
