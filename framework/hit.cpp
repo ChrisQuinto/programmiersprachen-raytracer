6eb741d60b6454e0f4e5a45dd247dfd45d17b582
ChrisQuinto <ch.paffrath@gmx.net>
2015-08-22 16:39:11 +0200

    adding hit.cpp + updating intersection methods (sphere)

---
+++ framework/hit.cpp
@@ -0,0 +1,45 @@
+#include "hit.hpp"
+#include "shape.hpp"
+
+Hit::Hit() :
+hit_{false},
+distance_{INFINITY},
+intersection_{glm::vec3{ INFINITY,INFINITY,INFINITY }},
+normal_{glm::vec3{ INFINITY,INFINITY,INFINITY }},
+sptr_{nullptr} {}
+
+Hit::Hit(bool hit, float dis, glm::vec3 const& intersection,
+    glm::vec3 const& norm, std::shared_ptr<Shape> sptr) :
+hit_{hit},
+distance_{dis},
+intersection_{intersection},
+normal_{norm},
+sptr_{sptr} {}
+
+Hit& Hit::operator=(Hit const& hit) {
+    if (this == &hit) {
+        return *this;
+    }
+
+    hit_ = hit.hit_;
+    distance_ = hit.distance_;
+    intersection_ = hit.intersection_;
+    normal_ = hit.normal_;
+    sptr_ = hit.sptr_;
+
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, Hit const& hit) {
+    os << "Hit: " << hit.hit_ << std::endl
+       << "Distanz: " << hit.distance_ << std::endl
+       << "Intersection: ("
+       << hit.intersection_.x << ", "
+       << hit.intersection_.y << ", "
+       << hit.intersection_.z << ")" << std::endl
+       << "Normal: ("
+       << hit.normal_.x << ", "
+       << hit.normal_.y << ", "
+       << hit.normal_.z << ")" << std::endl;
+    return os;
+}
--- framework/hit.hpp
+++ framework/hit.hpp
@@ -1,11 +1,24 @@
 #ifndef BUW_HIT_HPP
 #define BUW_HIT_HPP
 #include <glm/vec3.hpp>
+#include <memory>
 #include "ray.hpp"
 
+class Shape;
+
 struct Hit {
-    bool hit;
-    float distance;
+    Hit();
+    Hit(bool hit, float dis, glm::vec3 const& intersection, glm::vec3 const& norm, std::shared_ptr<Shape> sptr);
+
+    Hit& operator=(Hit const& hit);
+
+    friend std::ostream& operator<<(std::ostream& os, Hit const& hit);
+
+    bool hit_;
+    float distance_;
+    glm::vec3 intersection_;
+    glm::vec3 normal_; //Normalenvektor am Schnittpunkt
+    std::shared_ptr<Shape> sptr_;
 };
 
 #endif
