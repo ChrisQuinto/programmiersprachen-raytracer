ba7449b018837f05e6217d41b96f65601fb1b8d5
ChrisQuinto <ch.paffrath@gmx.net>
2015-08-23 17:20:23 +0200

    minor update

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -1,36 +1,33 @@
-
 #ifndef BUW_SCENE_HPP
 #define BUW_SCENE_HPP
 #include <map>
 #include <vector>
+#include <memory>
+#include <string>
+#include <iostream>
 #include "shape.hpp"
 #include "material.hpp"
 #include "camera.hpp"
 #include "renderer.hpp"
 #include "light.hpp"
-#include <memory>
-#include <iostream>
-//#include <string>
 
 struct Scene
 {
     Scene() :
     camera{Camera{}},
     amblight{Color{}},
-    background{Color{}},
-    renderer{Renderer{}}
-
-    {std::cout << "Scene built." << std::endl;}
+    background{Color{}} {
+        std::cout << "Scene built." << std::endl;
+    }
 
     ~Scene() {}
+
     std::map<std::string, Material> materials;
     std::vector<std::shared_ptr <Shape>> shapes;
     std::vector<Light> lights;
     Camera camera;
     Color amblight;
     Color background;
-    Renderer renderer;
-
 };
 
 #endif
