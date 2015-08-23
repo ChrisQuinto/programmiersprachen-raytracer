b066b61a8f27509f0fcee2f2e496f2fd61e12a14
ChrisQuinto <ch.paffrath@gmx.net>
2015-08-23 17:18:53 +0200

    moving resolution to camera + adding getter

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -3,11 +3,15 @@
 Camera::Camera() :
     name_{"defaultcam"},
     pos_{0.0,0.0,0.0},
+    xres_{500},
+    yres_{500},
     fovx_{60.0f} {}
 
-Camera::Camera(std::string const& n, float const& fovx) :
+Camera::Camera(std::string const& n,unsigned x,unsigned y, float const& fovx) :
     name_{n},
     pos_{0.0,0.0,0.0},
+    xres_{x},
+    yres_{y},
     fovx_{fovx} {}
 
 Camera::~Camera() {}
@@ -17,6 +21,12 @@ std::string const& Camera::name() const {
 }
 glm::vec3 const& Camera::pos() const {
     return pos_;
+}
+unsigned const& Camera::xres() const {
+    return xres_;
+}
+unsigned const& Camera::yres() const {
+    return yres_;
 }
 float const& Camera::fovx() const {
     return fovx_;
@@ -32,6 +42,7 @@ std::ostream& operator<<(std::ostream& os, Camera const& c)
     os << "Camera: \n"
        << "Name: " << c.name() << "\n"
        << "Pos: (" << c.pos().x << "," << c.pos().y << "," << c.pos().z << ")\n"
+       << "Auflösung: " << c.xres() << "x" << c.yres() << "\n"
        << "Winkel: "<< c.fovx() << "\n\n";
     return os;
   }
--- framework/camera.hpp
+++ framework/camera.hpp
@@ -10,11 +10,13 @@ class Camera
 {
 public:
     Camera();
-    Camera(std::string const& n, float const& fovx);
+    Camera(std::string const& n, unsigned x, unsigned y, float const& fovx);
     ~Camera();
 
     std::string const& name() const;
     glm::vec3 const& pos() const;
+    unsigned const& xres() const;
+    unsigned const& yres() const;
     float const& fovx() const;
     Ray const castray(glm::vec3 d);
 
@@ -23,6 +25,8 @@ public:
 private:
     std::string name_;
     glm::vec3 pos_;
+    unsigned xres_;
+    unsigned yres_;
     float fovx_;
 };
 
