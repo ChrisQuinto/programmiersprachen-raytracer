9a69c1a91ec0e7636062f9eb22552e40704e0b9a
ChrisQuinto <ch.paffrath@gmx.net>
2015-08-25 17:10:45 +0200

    structuring code

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -37,37 +37,34 @@ Renderer::Renderer(std::shared_ptr<Scene> scene) :
 
 void Renderer::render()
 {
-  unsigned z = (scene_->camera.xres()/2)/(tan(scene_->camera.fovx()/360 * M_PI));
-  for (unsigned y = 0; y < scene_->camera.yres(); ++y) {
-      for (unsigned x = 0; x < scene_->camera.xres(); ++x) {
-      glm::vec3 direction (-scene_->camera.xres()/2-0.5+x, -scene_->camera.yres()/2-0.5+y, z);
-      Ray ray = scene_->camera.castray(direction);
+    unsigned z = (scene_->camera.xres()/2)/(tan(scene_->camera.fovx()/360 * M_PI));
+    for (unsigned y = 0; y < scene_->camera.yres(); ++y) {
+        for (unsigned x = 0; x < scene_->camera.xres(); ++x) {
+            glm::vec3 direction (-scene_->camera.xres()/2-0.5+x, -scene_->camera.yres()/2-0.5+y, z);
+            Ray ray = scene_->camera.castray(direction);
 
-      for (std::vector<std::shared_ptr<Shape>>::iterator i =scene_->shapes_ptr.begin();i != scene_->shapes_ptr.end();++i){
-        *scene_->shapes_ptr[0].intersect(ray);
-      }
+            for (std::vector<std::shared_ptr<Shape>>::iterator i =scene_->shapes_ptr.begin();i != scene_->shapes_ptr.end();++i){
+                    *scene_->shapes_ptr[0].intersect(ray);
+            }
 
-      Pixel p(x,y);
-
-
-      write(p);
+        Pixel p(x,y);
+        write(p);
+        }
     }
-  }
-  ppm_.save();
+    ppm_.save();
 }
 
 void Renderer::write(Pixel const& p)
 {
-  // flip pixels, because of opengl glDrawPixels
+    // flip pixels, because of opengl glDrawPixels
     size_t buf_pos = (scene_->camera.xres()*p.y + p.x);
-  if (buf_pos >= colorbuffer_.size() || (int)buf_pos < 0) {
-    std::cerr << "Fatal Error Renderer::write(Pixel p) : "
-      << "pixel out of ppm_ : "
-      << (int)p.x << "," << (int)p.y
-      << std::endl;
-  } else {
-    colorbuffer_[buf_pos] = p.color;
-  }
-
-  ppm_.write(p);
+    if (buf_pos >= colorbuffer_.size() || (int)buf_pos < 0) {
+        std::cerr << "Fatal Error Renderer::write(Pixel p) : "
+                  << "pixel out of ppm_ : "
+                  << (int)p.x << "," << (int)p.y
+                  << std::endl;
+    } else {
+        colorbuffer_[buf_pos] = p.color;
+    }
+    ppm_.write(p);
 }
