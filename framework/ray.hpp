37990270166a59afb87bcc186e45f004413324fa
ChrisQuinto <ch.paffrath@gmx.net>
2015-08-26 17:30:47 +0200

    adding inv_direction

--- framework/ray.hpp
+++ framework/ray.hpp
@@ -6,16 +6,19 @@ struct Ray
 {
     Ray() :
     origin_{0.0,0.0,0.0},
-    direction_{0.0,0.0,0.0} {}
+    direction_{0.0,0.0,0.0},
+    inv_direction_{-0.0,-0.0,-0.0} {}
 
     Ray(glm::vec3 const& o, glm::vec3 const& d) :
     origin_{o},
-    direction_{d} {}
+    direction_{d},
+    inv_direction_{1.0/d.x,1.0/d.y,1.0/d.z} {}
 
     ~Ray() {}
 
     glm::vec3 origin_;
     glm::vec3 direction_;
+    glm::vec3 inv_direction_;
 };
 
 #endif
